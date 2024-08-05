6b9a3c1c1603a0c530db7e9e14659a5bcefa6b89
Do Long Duy <146762887+DoLongDuy@users.noreply.github.com>
2024-08-06 01:50:52 +0700

    Add files via upload

--- fibonancy.py
+++ fibonancy.py
@@ -41,7 +41,45 @@ def sumation_test_case():
     print("Sumation test case")
     print(test_summation_property())
 
+def test_shift_property_mutant1():
+    fib_10 = fibonacci(10)
+    fib_11 = fibonacci(11)
+    if fib_11 == [fib_10[0], fib_10[1], fib_10[2], fib_10[3], fib_10[4], fib_10[5], fib_10[6], fib_10[7], fib_10[8], fib_10[9]]:
+        return True
+    else:
+        return False
+
+def test_shift_property_mutant2():
+    fib_10 = fibonacci(10)
+    fib_11 = fibonacci(11)
+    if fib_11 == [fib_10[0], fib_10[1], fib_10[2], fib_10[3], fib_10[4], fib_10[5], fib_10[6], fib_10[7], fib_10[8], fib_10[10]]:
+        return True
+    else:
+        return False
+    
+def test_summation_property_mutant1():
+    fib_10 = fibonacci(10)
+    fib_sum = sum(fib_10)
+    if fib_sum == fibonacci(12)[-1]:
+        return True
+    else:
+        return False
+
+def test_mutation_analysis(analysis_type):
+    if analysis_type == 1:
+        print("Shift property mutation 1: This mutant removes the last element of the 11-term sequence, which should not pass the test case.")
+        print(test_shift_property_mutant1())
+    elif analysis_type == 2:
+        print("Shift property mutation 2: This mutant removes the last element of the 11-term sequence, which should not pass the test case.")
+        print(test_shift_property_mutant2())
+    elif analysis_type == 3:
+        print("Summation property mutation: This mutant checks if the sum of the first 10 Fibonacci numbers is equal to the 12th Fibonacci number, which should not pass the test case.")
+        print(test_summation_property_mutant1())
+    else:
+        print("???")
 
 shift_test_case(10, 11 )  # Output: [0, 1, 1, 2, 3, 5, 8, 13, 21, 34]
 print()
 sumation_test_case()
+print()
+
